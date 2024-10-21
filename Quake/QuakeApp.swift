501f3c497bed622e408c8d0302fb34c341f3f189
Reid Allenstein <reidallenstein@gmail.com>
2024-10-21 11:53:15 -0700

    Initial Commit

---
+++ Quake/QuakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuakeApp.swift
+//  Quake
+//
+//  Created by Reid Allenstein on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct QuakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuakeTests/QuakeTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuakeTests.swift
+//  QuakeTests
+//
+//  Created by Reid Allenstein on 10/14/24.
+//
+
+import Testing
+@testable import Quake
+
+struct QuakeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
